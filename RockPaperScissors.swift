657c7d0d780ad359c3558986fcf9ba5564320070
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-07 04:19:26 +0000

    Fixed comments

--- RockPaperScissors.swift
+++ RockPaperScissors.swift
@@ -8,13 +8,17 @@
    display the result. 
 */
 
+// Array of possible moves
 let moves = ["rock", "paper", "scissors"]
+
 while true {
+	// Ask user for a move and reads input
 	print("Please enter either 0(rock), 1(paper), or 2(scissors): ")
 	let playerNum = Int(readLine()!)!
 	let computerNum = Int.random(in: 0..<3)
 	let computerMove = moves[computerNum]
 	if let playerMove = Optional(moves[playerNum]) {
+		// Compare player’s move with computer’s move and displays the result
 		if playerNum == computerNum {
 			print("Player picked \(playerMove) and computer picked \(computerMove), so it's a tie!")
 		} else if (playerNum > computerNum) && !(playerNum == 2 && computerNum == 0)
